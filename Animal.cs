41d433854fe46244a2568e001109e205f117f451
shewall <92726100+shewall@users.noreply.github.com>
2023-05-03 02:44:52 +0100

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace consoleapp
+{
+    public class Animal
+    {
+        public string Name { get; }
+        public double AnimalCount { get; }
+        public double NutCount { get; }
+        public double FeedingTime { get; }
+        public double SecondPerNut { get; }
+
+        public Animal()
+        {
+            Console.WriteLine("First you will enter the animal's attributes.");
+            //  Getting the name attribute : sincap
+            Console.WriteLine("Enter animal name");
+            Name = Console.ReadLine();
+            //  Obtaining animal quantity attribute  : 1.5
+            Console.WriteLine("Enter animal count");
+            AnimalCount = Convert.ToDouble(Console.ReadLine());
+            //  Obtaining hazelnut quantity quality : 1.5
+            Console.WriteLine("Enter nut count");
+            NutCount = Convert.ToDouble(Console.ReadLine());
+            //  Fındıkların yenme süresinin alınması : 90seconds
+            Console.WriteLine("Enter feeding time");
+            FeedingTime = Convert.ToDouble(Console.ReadLine());
+            // Calculating the time it takes a soldering iron to eat a nut
+            SecondPerNut = FeedingTime / (NutCount / AnimalCount);
+        }
+
+        // Calculation of the number of hazelnuts eaten by the given animal within the given time
+        public double CalculateNuts()
+        {
+            // Getting the given time in minutes from the console
+            Console.WriteLine("Enter the total time in minute version of the" + Name + "'s feeding time:");
+            // Converting the given time to seconds
+            double givenFeedingTime = double.Parse(Console.ReadLine()) * 60;
+            // Receiving the total number of animals
+            Console.WriteLine("Enter the total animal count:");
+            double givenAnimalCount = double.Parse(Console.ReadLine());
+            // Calculation of the total number of nuts eaten
+            return givenAnimalCount * (givenFeedingTime / SecondPerNut);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many different animals will you enter? ");
+            int animalCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < animalCount; i++)
+            {
+                var animal = new Animal();
+                // Printing the result to the screen
+                Console.WriteLine("Answer for " + animal.Name + " : " + animal.CalculateNuts());
+                Console.WriteLine("----------------------------------------");
+            }
+        }
+    }
+}
